6a0e27c37bb2a84dd878e7ad68ef026e1100b11c
jamal-pb95 <jamal.pb95@gmail.com>
2019-01-09 00:08:36 +0600

    Updates

---
+++ precache-manifest.b6c541345940a299e32a201bc1085cb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "beae02f73a97e34adfbf",
+    "url": "/city-tour/static/js/runtime~main.beae02f7.js"
+  },
+  {
+    "revision": "78ca4246a3c12f5c8b0c",
+    "url": "/city-tour/static/js/main.78ca4246.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/city-tour/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "78ca4246a3c12f5c8b0c",
+    "url": "/city-tour/static/css/main.5a89ee29.chunk.css"
+  },
+  {
+    "revision": "6191dc0aa45020f7fef4cfbf2ed5d17e",
+    "url": "/city-tour/index.html"
+  }
+];
